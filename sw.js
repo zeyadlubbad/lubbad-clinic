f1d2a2ba955075256efe6156798614ef91091f44
zeyadlubbad <146716643+zeyadlubbad@users.noreply.github.com>
2026-08-16 17:49:24 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='clinic-v4';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
